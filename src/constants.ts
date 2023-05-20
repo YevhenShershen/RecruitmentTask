70ba578a4fa4de308d18b60ef447d388ab3792c4
Yevhen <shershen.yevhen@gmai>
2023-05-20 10:41:27 +0200

    create type

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,1 @@
-export const URL_USERS = 'https://jsonplaceholder.typicode.com/users'
+export const URL = ''
---
+++ src/types.ts
@@ -0,0 +1,8 @@
+export type Product = {
+  id: String
+  name: String
+  category: boolean
+  price: number
+  animal: String
+}
+//category: true i false; true = domki na drzewach, false = domki na ziemi
