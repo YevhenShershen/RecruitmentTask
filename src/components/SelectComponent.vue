a067fea96bd277d5e72cdc6954f46ff4ef77a2d2
Yevhen <shershen.yevhen@gmai>
2023-05-21 21:47:40 +0200

    create component SelectComponent.vue

---
+++ src/components/SelectComponent.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+type PropTypes = {
+  items: String[]
+  label: String
+}
+const props = defineProps<PropTypes>()
+const emit = defineEmits<{
+  (e: 'return-current-item', item: String): void
+}>()
+const returnItem = () => {
+  emit('return-current-item', currentItem.value)
+}
+const currentItem = ref('')
+</script>
+<template>
+  <v-select
+    v-model="currentItem"
+    :items="props.items"
+    :label="props.label"
+    required
+    @update:modelValue="returnItem()"
+  ></v-select>
+</template>
