2434779043266726152a688de190f327d99318cf
Yevhen <shershen.yevhen@gmai>
2023-05-25 20:10:19 +0200

    create component AddCategory.vue

---
+++ src/components/admin-page/AddCategory.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { onMounted, ref } from 'vue'
+</script>
+
+<template>
+  <div class="product-table d-flex align-center">
+    <v-text-field label="dodaj kategorije" type="text" hide-details="auto"></v-text-field>
+    <v-btn class="ml-5">Dodaj kategorije</v-btn>
+  </div>
+</template>
+
+<style scoped lang="scss"></style>
